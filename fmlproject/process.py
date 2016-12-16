4c76dc736fc6253cad5f12f33f3ff675468c3e7d
pratheeksh <pks329@nyu.edu>
2016-12-16 18:39:51 -0500

    refactor

--- fmlproject/process.py
+++ fmlproject/process.py
@@ -4,13 +4,13 @@ import os
 labels = ['cla', 'gac', 'org', 'sax', 'vio', 'cel', 'flu', 'gel', 'pia', 'tru', 'voi']
 
 for l in labels:
-    parent_dir = "/Users/pratheeksha/School/fml/data/IRMAS-TrainingData/" + l
+    parent_dir = "/home/pratheeksha/fml/data/IRMAS-TrainingData/" + l
     m = dict()
     m['label'] = l
     for file in os.listdir(parent_dir):
         y, sr = librosa.load(parent_dir +"/"+ file)
         m[file] = librosa.feature.mfcc(y=y, sr=sr)
-    savemat("/Users/pratheeksha/School/FML/data/matfiles"+file+".mat", m)
+    savemat("/home/pratheeksha/fml/data/matfiles/"+l+".mat", m)
     print "Processed label" + l
 
 
